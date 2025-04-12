47e3cf8561c418d02773b2ebe262c4ba42f97cf2
Jonathang <jgarcia2888@gmail.com>
2025-04-12 13:35:54 -0600

    cascaron

---
+++ src/main/java/com/cliente/pld/OrqObtienePLD/OrqObtienePldApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.pld.OrqObtienePLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrqObtienePldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrqObtienePldApplication.class, args);
+	}
+
+}
